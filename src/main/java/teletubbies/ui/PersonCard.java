0b35b95bf24fe6b4e00232d77a054421b59fed41
lhw-1 <leehyungwoonsamuel@gmail.com>
2021-10-26 21:42:27 +0800

    Fix CheckStyle violations

--- src/main/java/teletubbies/ui/PersonCard.java
+++ src/main/java/teletubbies/ui/PersonCard.java
@@ -4,12 +4,9 @@ import java.nio.charset.StandardCharsets;
 import java.util.Comparator;
 
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.layout.FlowPane;
